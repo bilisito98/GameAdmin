a7694a19e607a565a896f2418d38d4a60bcedf6a
william Vargas <102624868+bilisito98@users.noreply.github.com>
2025-10-14 16:17:46 -0500

    CommitPage_GameAdmin_1

--- src/components/LoginModal.vue
+++ src/components/LoginModal.vue
@@ -54,7 +54,7 @@ const close = () => emit('close')
 
 const doLogin = async () => {
   error.value = null
-  const apiBase = 'http://localhost:5147' // 🔗 tu backend ASP.NET
+  const apiBase = import.meta.env.VITE_API_URL || 'https://gameadmin-backend-1.onrender.com'
 
   try {
     // login del store

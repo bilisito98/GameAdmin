bcf2fdb128a6993878e3dc21907c6b3aabc31050
william Vargas <102624868+bilisito98@users.noreply.github.com>
2025-10-31 14:17:41 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,11 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// Configuración para Vue
 export default defineConfig({
-  plugins: [vue()],
-  server: {
-    port: 5173,
-    host: true
-  },
+  plugins: [ vue() ],
   build: {
-    outDir: 'dist'
+    // opcional: aumenta tamaño de chunk si tuvieses problemas
+    chunkSizeWarningLimit: 2000
   }
 })

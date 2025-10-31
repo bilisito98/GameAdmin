36f0b1239400e1c51e029ad94500f786ccf137ae
william Vargas <102624868+bilisito98@users.noreply.github.com>
2025-10-31 14:24:54 -0500

    Configure server port and update build options
    Added server configuration and updated build settings.

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,16 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [ vue() ],
+  plugins: [vue()],
+  server: {
+    port: 5173
+  },
   build: {
-    // opcional: aumenta tamaño de chunk si tuvieses problemas
-    chunkSizeWarningLimit: 2000
+    outDir: 'dist'
+  },
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
   }
 })

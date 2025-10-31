202a1529d8e8d4113b72193564dc4972b0a39213
william Vargas <102624868+bilisito98@users.noreply.github.com>
2025-10-31 14:35:26 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,15 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    port: 5173
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
   },
   build: {
     outDir: 'dist'
-  },
-  resolve: {
-    alias: {
-      '@': '/src'
-    }
   }
 })
